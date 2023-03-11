2a9036a497b36bc10a00e90a958635b72837fd59
Tony Trinh <tonytrinh911@gmail.com>
2023-03-11 15:16:54 -0500

    added manager question array

--- index.js
+++ index.js
@@ -4,3 +4,27 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+const managerQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the team manager?'
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: 'What is the ID number of the team manager?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email of the team manager?'
+    },
+    {
+        type: 'input',
+        name: 'officeNum',
+        message: 'What is the office number of the team manager?'
+    }
+];
+
