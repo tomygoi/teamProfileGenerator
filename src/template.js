bab3f75fe9e205f07bde94616c41f345e8315c40
Tony Trinh <tonytrinh911@gmail.com>
2023-03-11 19:56:58 -0500

    added html template for page setup and style sheet with basic styles

--- src/template.js
+++ src/template.js
@@ -72,3 +72,37 @@ function buildSite(data) {
     }) 
     return site
 }
+
+const generateHTML = (data) => {
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../dist/assets/styles.css">
+    <title>Team Profile Generator</title>
+</head>
+<body>
+    <header>
+        <div class = "header-container">
+            <div class = "row">
+                <h1 class = "page-header">My Team</h1>
+            </div>
+        </div>
+    </header>
+</body>
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="team">
+                ${buildSite(data)}
+            </div>
+        </div>
+    </div>
+</section>
+</html>
+    `;
+};
+module.exports = generateHTML;
